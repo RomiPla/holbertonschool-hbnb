ad5654beac4429ff422bf9e340f48b8fcb8eec42
GaboAfk <gabrielmorffe95@gmail.com>
2024-06-11 12:49:46 -0300

    unique email by user and condition for host in place
    Co-authored-by: ManuAlePacheRu <ManuAlePacheRu@users.noreply.github.com>

--- model/place.py
+++ model/place.py
@@ -1,8 +1,12 @@
 from basemodel import Basemodel
+from user import User
 
 class Place(Basemodel):
-    def __init__(self, name, description, number_of_rooms, number_of_bathrooms, max_guests, price_by_nigth, latitude, longitude, City):
+    def __init__(self, host_id, name, description, number_of_rooms, number_of_bathrooms,\
+                 max_guests, price_by_nigth, latitude, longitude, City):
         super().__init__()
+        if host_id not in User.user_emails:
+            raise ValueError(f"Host with id {host_id} does not exist.")
         self.name = name
         self.description = description
         self.number_of_rooms = number_of_rooms 
--- model/user.py
+++ model/user.py
@@ -1,13 +1,22 @@
 from basemodel import Basemodel
 
 class User(Basemodel):
+    user_emails = {}
     def __init__(self, email, first_name, last_name):
         super().__init__()
+        if email in User.user_emails.values():
+            raise ValueError(f"Email {email} already in use.")
+
+        """if email is @:
+            self.email = email
+        else:
+            raise TypeError("email de vrg")"""
         self.email = email
         self.first_name = first_name
         self.last_name = last_name
         self.places = []
         self.reviews = []
+        User.user_emails[self.id] = email
 
     def add_place(self, Place):
         self.places.append(Place)
