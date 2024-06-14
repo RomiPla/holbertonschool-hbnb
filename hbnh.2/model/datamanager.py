d2125e8a33eee15a0cb31c375597554d37aaf0ed
GaboAfk <gabrielmorffe95@gmail.com>
2024-06-14 05:57:32 -0300

    Ajuste formato data, update fix

--- hbnh.2/model/datamanager.py
+++ hbnh.2/model/datamanager.py
@@ -2,28 +2,40 @@ from IPersistenceManager import IPersistenceManager
 import datetime
 
 class DataManager(IPersistenceManager):
-    def __init__(self):
-        self.data = {}
+    #def __init__(self):
+    #    self.data = {}
 
+    data = {}
+
+    @classmethod
     def save(self, entity):
         entity_type = type(entity).__name__
         if entity_type not in self.data:
             self.data[entity_type] = {}
         self.data[entity_type][entity.id] = entity
 
+    @classmethod
     def get(self, entity_id, entity_type):
         if entity_type in self.data:
             return self.data[entity_type].get(entity_id)
         return None
+    
+    @classmethod
+    def get_all(self, entity_type):
+        if entity_type in self.data:
+            return self.data[entity_type]
+        return None
 
+    @classmethod
     def update(self, entity):
         entity_type = type(entity).__name__
         if entity_type in self.data:
             entity_id = entity.id
             if entity_id in self.data[entity_type]:
                 entity.updated_at = datetime.datetime.now()
-                self.data[entity_type][entity_id] = entity
+                #self.data[entity_type][entity_id] = entity
 
+    @classmethod
     def delete(self, entity_id, entity_type):
         if entity_type in self.data:
             if entity_id in self.data[entity_type]:
